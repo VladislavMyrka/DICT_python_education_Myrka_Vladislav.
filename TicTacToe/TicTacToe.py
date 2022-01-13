0de67953c6cce59914fb0663f28df34c604e0b04
Владислав_Мирка <v.o.myrka@student.khai.edu>
2022-01-13 13:07:05 +0300

    TicTacToe_update

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,75 +1,114 @@
-def print_tic_tac_toe(values):
-    print("\n")
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]))
-    print('\t_____|_____|_____')
+import random
 
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]))
-    print('\t_____|_____|_____')
 
-    print("\t     |     |")
+board = [i for i in range(0, 9)]
+player, computer = '', ''
 
-    print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
-    print("\t     |     |")
-    print("\n")
 
-    def single_game(cur_player):
+moves = ((1, 7, 3, 9), (5,), (2, 4, 6, 8))
 
-        values = [' ' for x in range(9)]
+winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 
-        player_pos = {'X': [], 'O': []}
+tab = range(1, 10)
 
-        while True:
-            print_tic_tac_toe(values)
 
-            try:
-                print("Player ", cur_player, " turn. Which box? : ", end="")
-                move = int(input())
-            except ValueError:
-                print("Wrong Input!!! Try Again")
-                continue
+def print_board():
+    x = 1
+    for i in board:
+        end = ' | '
+        if x % 3 == 0:
+            end = ' \n'
+            if i != 1: end += '---------\n';
+        char = ' '
+        if i in ('X', 'O'): char = i;
+        x += 1
+        print(char, end=end)
 
-            # Sanity check for MOVE inout
-            if move < 1 or move > 9:
-                print("Wrong Input!!! Try Again")
-                continue
 
-            # Check if the box is not occupied already
-            if values[move - 1] != ' ':
-                print("Place already filled. Try again!!")
-                continue
+def select_char():
+    chars = ('X', 'O')
+    if random.randint(0, 1) == 0:
+        return chars[::-1]
+    return chars
 
-                values[move - 1] = cur_player
 
-                player_pos[cur_player].append(move)
+def can_move(brd, player, move):
+    if move in tab and brd[move - 1] == move - 1:
+        return True
+    return False
 
-                if check_win(player_pos, cur_player):
-                    print_tic_tac_toe(values)
-                    print("Player ", cur_player, " has won the game!!")
-                    print("\n")
-                    return cur_player
 
-                if check_draw(player_pos):
-                    print_tic_tac_toe(values)
-                    print("Game Drawn")
-                    print("\n")
-                    return 'D'
+def can_win(brd, player, move):
+    places = []
+    x = 0
+    for i in brd:
+        if i == player: places.append(x);
+        x += 1
+    win = True
+    for tup in winners:
+        win = True
+        for ix in tup:
+            if brd[ix] != player:
+                win = False
+                break
+        if win == True:
+            break
+    return win
 
-                def check_win(player_pos, cur_player):
-                    soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-                    for x in soln:
-                        if all(y in player_pos[cur_player] for y in x):
-                            return True
-                        return False
 
-                    def check_draw(player_pos):
-                        if len(player_pos['X']) + len(player_pos['O']) == 9:
-                            return True
-                            return False
+def make_move(brd, player, move, undo=False):
+    if can_move(brd, player, move):
+        brd[move - 1] = player
+        win = can_win(brd, player, move)
+        if undo:
+            brd[move - 1] = move - 1
+        return (True, win)
+    return (False, False)
 
-                        if cur_player == 'X':
-                            cur_player = 'O'
-                        else:
-                            cur_player = 'X'
 
+
+def computer_move():
+    move = -1
+    for i in range(1, 10):
+        if make_move(board, computer, i, True)[1]:
+            move = i
+            break
+    if move == -1:
+        for i in range(1, 10):
+            if make_move(board, player, i, True)[1]:
+                move = i
+                break
+    if move == -1:
+        for tup in moves:
+            for mv in tup:
+                if move == -1 and can_move(board, computer, mv):
+                    move = mv
+                    break
+    return make_move(board, computer, move)
+
+
+def space_exist():
+    return board.count('X') + board.count('O') != 9
+
+
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
+result = '%%% Ничья! %%%'
+while space_exist():
+    print_board()
+    print('# Make your move ! [1-9] : ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print(' >> Invalid number ! Try again !')
+        continue
+    #
+    if won:
+        result = '*** Congratulations ! You won ! ***'
+        break
+    elif computer_move()[1]:
+        result = '=== You lose ! =='
+        break;
+
+print_board()
+print(result)
