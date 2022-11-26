5b71da40847332697345c7465184ed355490e41d
VladislavMyrka <v.o.myrka@student.khai.edu>
2022-11-26 19:40:00 +0300

    test

---
+++ hacks/hack3.py
@@ -0,0 +1,92 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+data = b'secret data'
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+file_out.close()
+
+from Crypto.Cipher import AES
+
+file_in = open("encrypted.bin", "rb")
+nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+data = cipher.decrypt_and_verify(ciphertext, tag)
+
+from Crypto.PublicKey import RSA
+
+secret_code = "Unguessable"
+key = RSA.generate(2048)
+encrypted_key = key.export_key(passphrase=secret_code, pkcs=8,
+                              protection="scryptAndAES128-CBC")
+
+file_out = open("rsa_key.bin", "wb")
+file_out.write(encrypted_key)
+file_out.close()
+
+print(key.publickey().export_key())
+
+from Crypto.PublicKey import RSA
+
+secret_code = "Unguessable"
+encoded_key = open("rsa_key.bin", "rb").read()
+key = RSA.import_key(encoded_key, passphrase=secret_code)
+
+print(key.publickey().export_key())
+
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey().export_key()
+file_out = open("receiver.pem", "wb")
+file_out.write(public_key)
+file_out.close()
+
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+file_out = open("encrypted_data.bin", "wb")
+
+recipient_key = RSA.import_key(open("receiver.pem").read())
+session_key = get_random_bytes(16)
+
+# Encrypt the session key with the public RSA key
+cipher_rsa = PKCS1_OAEP.new(recipient_key)
+enc_session_key = cipher_rsa.encrypt(session_key)
+
+# Encrypt the data with the AES session key
+cipher_aes = AES.new(session_key, AES.MODE_EAX)
+ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+[ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+file_out.close()
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+file_in = open("encrypted_data.bin", "rb")
+
+private_key = RSA.import_key(open("private.pem").read())
+
+enc_session_key, nonce, tag, ciphertext = \
+   [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+
+cipher_rsa = PKCS1_OAEP.new(private_key)
+session_key = cipher_rsa.decrypt(enc_session_key)
+
+
+cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+print(data.decode("utf-8"))
